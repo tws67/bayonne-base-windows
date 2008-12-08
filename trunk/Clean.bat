5718ab8ff668c6c7d5c3ffcfd356abc2fa599f4a
tws67@21cn.com <tws67@21cn.com@cf240a98-c4e8-11dd-aab6-efc349ba664b>
2008-12-08 15:01:14 +0000

    清理了一些旧的工程备份文

--- trunk/Clean.bat
+++ trunk/Clean.bat
@@ -5,6 +5,7 @@ del fox-1.7.18\*.bak /s
 del fox-1.7.18\*.pdb /s
 del fox-1.7.18\*.log /s
 del fox-1.7.18\*.obj /s 
+del fox-1.7.18\*.old /s 
 echo clear the project : fox-1.7.18 end ......
 
 echo clear the project : exosip begin ......
@@ -12,6 +13,7 @@ del exosip\*.bak /s
 del exosip\*.pdb /s
 del exosip\*.log /s
 del exosip\*.obj /s 
+del exosip\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : ccrtp-1.6.2 begin ......
@@ -19,6 +21,7 @@ del ccrtp-1.6.2\*.bak /s
 del ccrtp-1.6.2\*.pdb /s
 del ccrtp-1.6.2\*.log /s
 del ccrtp-1.6.2\*.obj /s 
+del ccrtp-1.6.2\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : commoncpp2-1.6.3 begin ......
@@ -26,6 +29,7 @@ del commoncpp2-1.6.3\*.bak /s
 del commoncpp2-1.6.3\*.pdb /s
 del commoncpp2-1.6.3\*.log /s
 del commoncpp2-1.6.3\*.obj /s 
+del commoncpp2-1.6.3\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : ccscript3-0.8.0 begin ......
@@ -33,6 +37,7 @@ del ccscript3-0.8.0\*.bak /s
 del ccscript3-0.8.0\*.pdb /s
 del ccscript3-0.8.0\*.log /s
 del ccscript3-0.8.0\*.obj /s 
+del ccscript3-0.8.0\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : ccaudio2-0.9.5 begin ......
@@ -40,6 +45,7 @@ del ccaudio2-0.9.5\*.bak /s
 del ccaudio2-0.9.5\*.pdb /s
 del ccaudio2-0.9.5\*.log /s
 del ccaudio2-0.9.5\*.obj /s 
+del ccaudio2-0.9.5\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : bayonne2-2.3.2 begin ......
@@ -47,6 +53,7 @@ del bayonne2-2.3.2\*.bak /s
 del bayonne2-2.3.2\*.pdb /s
 del bayonne2-2.3.2\*.log /s
 del bayonne2-2.3.2\*.obj /s 
+del bayonne2-2.3.2\*.old /s 
 echo clear the project : exosip end ......
 
 echo clear the project : bayonne2-2.0.2 begin ......
@@ -54,4 +61,5 @@ del bayonne2-2.0.2\*.bak /s
 del bayonne2-2.0.2\*.pdb /s
 del bayonne2-2.0.2\*.log /s
 del bayonne2-2.0.2\*.obj /s 
+del bayonne2-2.0.2\*.old /s 
 echo clear the project : exosip end ......
